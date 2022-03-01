29d209258249db2c73ded3278cdb730e079acd66
mlbarre <mlbarrera1538@gmail.com>
2022-03-01 02:20:44 -0600

    added api routes to routes folder and imported it index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
+//import all api routes from api/index.js
+const apiRoutes = require('./api');
 const htmlRoutes = require('./html/html-routes');
 
+// add prefix of /api to all of the api route imported
+router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
 
 router.use((req, res) => {
