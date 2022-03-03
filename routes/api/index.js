0c18130c2c16ee01b2cd9612601aeca54ca8e8dc
mlbarre <mlbarrera1538@gmail.com>
2022-03-03 00:09:57 -0600

    imported comment-routes into index.js next to test

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes.js');
+const commentRoutes = require('./comment-routes.js');
 
+router.use('/comments', commentRoutes);
 //add prefix of '/pizzas' to routes creates in 'pizza-routes.js'
 router.use('/pizzas', pizzaRoutes);
 
